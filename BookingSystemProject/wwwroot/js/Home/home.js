490ddc99838504ea205672a6ad238b86a9bf2650
Rosen Yordanov <roskata182003@gmail.com>
2023-06-27 18:41:13 +0300

    I have done the homepage design and logic.

--- BookingSystemProject/wwwroot/js/Home/home.js
+++ BookingSystemProject/wwwroot/js/Home/home.js
@@ -1,4 +1,39 @@
-﻿const secondPicturePath = document.getElementById("second-img-path");
-const imgElement = document.querySelector(".hotel-container .hotel-container a img");
-const originalSrc = imgElement.src;
-console.log(originalSrc);
+﻿let secondPicturesPathElements = Array.from(document.querySelectorAll(".second-img-path"));
+let orginalImgsSrcArrayElements = Array.from(document.querySelectorAll(".hotel-img-container .original-img-path"));
+
+const originalImgsPath = orginalImgsSrcArrayElements.map((el) => el.textContent);
+const secondPicturesPath = secondPicturesPathElements.map((el) => el.textContent);
+
+const headerElement = document.querySelector("header");
+console.log(headerElement);
+window.addEventListener("scroll", () => {
+    headerElement.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
+const containers = document.querySelectorAll(".hotel-img-container img").forEach((container) => {
+    container.addEventListener("mouseover", changeImg);
+    container.addEventListener("mouseout", restoreImg);
+});
+
+
+function changeImg(event) {
+    let childrenArray = Array.from(event.target.parentElement.parentElement.parentElement.parentElement.children);
+    const index = childrenArray.indexOf(event.target.parentElement.parentElement.parentElement);
+    event.target.style.opacity = 0.7;
+    setTimeout(() => {
+        event.target.src = secondPicturesPath[index];
+        event.target.style.opacity = 1; 
+    }, 200);
+    
+}
+
+function restoreImg(event) {
+    let childrenArray = Array.from(event.target.parentElement.parentElement.parentElement.parentElement.children);
+    const index = childrenArray.indexOf(event.target.parentElement.parentElement.parentElement);
+    event.target.style.opacity = 0.7;
+    setTimeout(() => {
+        event.target.src = originalImgsPath[index];
+        event.target.style.opacity = 1;
+    }, 360);
+}
